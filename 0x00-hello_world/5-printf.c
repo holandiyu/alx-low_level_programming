7823b8984a9330cc5201c547342a8ba7200a6fc3
mustapha <mfou7247@gmail.com>
2023-06-17 01:08:04 +0100

    hu

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,8 +7,9 @@
  *
  * return: Always 0 (success)
 */
+
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n)";
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
